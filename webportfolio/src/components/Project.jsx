c6f9fe547c0b230e8ef5c4c82d0ad2a5d82936e2
Loui Lindkvist Nielsen <loui.apa@gmail.com>
2023-03-14 14:03:02 +0100

    img fix 3

--- webportfolio/src/components/Project.jsx
+++ webportfolio/src/components/Project.jsx
@@ -7,10 +7,9 @@ import '../assets/vader.png'
 import '../App.css'
 
 export default function Project(props) {
-    console.log(props)
     return (
         <div className={`project ${props.order ? '' : 'reverse--flex'}`} >
-             <div className='project--imgWrapper'><a target="_blank" href={props.url}><img className='project--img' src={props.img} alt="" /></a></div>
+             <div className='project--imgWrapper'><a target="_blank" href={props.url}><img className='project--img' src={`src/assets/${props.img}`} alt="" /></a></div>
             <div className={`project--content ${props.order ? '' : 'reverse--content'}`} >
                 <a target='_blank   ' href={props.url}><h1 className={`project--title ${props.title}`}>{props.title}</h1></a>
                 <p className='project--desc'>{props.description}</p>
--- webportfolio/src/projectData.js
+++ webportfolio/src/projectData.js
@@ -1,13 +1,11 @@
-import blog from './assets/blog.png'
-import catchFilip from './assets/catchfilip3.png'
-import vader from './assets/vader.png'
+
 
 export default [
     {
         id: 1,
         title: 'Catch Filip',
         description: 'Catch Filip är ett roligt och snabbt spel där spelaren tar på sig rollen som en huvudjägare och målet är att fånga så många av Filips huvud som möjligt. Huvudena faller i olika hastigheter, vilket gör att spelaren måste vara snabb och uppmärksam för att fånga dem alla. Spelet blir mer utmanande ju längre spelaren spelar, ju längre spelet går, ju snabbare skapas nya huvden som spelaren måste fånga. (Använd A och D för att styra korgen)',
-        img: catchFilip,
+        img: 'catchfilip3.png',
         url: 'https://catchfilip.netlify.app/',
         verktyg: ['HTML', 'CSS', 'JavaScript'],
         order: 1,
@@ -17,7 +15,7 @@ export default [
         id: 2,
         title: 'Väder-app',
         description: 'En Webbapplikation som visar väderinformation för platser i Sverige för både nuvarande och kommande 10 dagar. Användaren kan söka efter en ort genom att ange ortens namn i en sökruta. När användaren väljer en ort, hämtar applikationen väderinformation från SMHI:s API och visar den på skärmen. Applikationen är användarvänlig, responsiv och lätt att använda',
-        img: vader,
+        img: 'vader.png',
         url: 'https://glittery-sherbet-6df2ab.netlify.app/',
     
         verktyg: ['HTML', 'CSS', 'JavaScript', 'PHP'],
@@ -29,7 +27,7 @@ export default [
         id: 3,
         title: '8-bit Blog',
         description: 'Blogg där användare kan logga in och skriva inlägg som sedan publiceras på Bloggens förstasida. Från förstasidan är det också möjligt att navigera till enskilda inlägg',
-        img: blog,
+        img: 'blog.png',
         url: 'https://melab.lnu.se/~ll223vk/Blogg/index.php',
 
         verktyg: ['HTML', 'CSS', 'JavaScript', 'PHP'],

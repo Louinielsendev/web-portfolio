378f336bc27e6d56feefa056d14f8a2fb2b820ea
Loui Lindkvist Nielsen <loui.apa@gmail.com>
2024-03-24 15:57:40 +0100

    new projects and favicon added

--- webportfolio/src/components/Project.jsx
+++ webportfolio/src/components/Project.jsx
@@ -1,5 +1,7 @@
 import githubLogo from '../assets/github.svg'
 import githubLogoWhite from '../assets/github-white.svg'
+import webicon from '../assets/website.png'
+import webiconWhite from '../assets/websiteWhite.png'
 
 
 import '../App.css'
@@ -14,7 +16,9 @@ export default function Project(props) {
                 <p className='project--desc'>{props.description}</p>
                 <div className='project--btn'>
                 <a target='_blank' href={props.github}><img className='logo'src={props.darkMode ? githubLogoWhite : githubLogo} alt="" /></a>
+                <a target='_blank' href={props.url}><img className='logo'src={props.darkMode ? webiconWhite : webicon} alt="" /></a>
                 </div>
+                
             </div>
            
         </div>
--- webportfolio/src/projectData.js
+++ webportfolio/src/projectData.js
@@ -2,16 +2,42 @@ import blog from'./assets/blog.png'
 import catchFilip from './assets/catchfilip3.png'
 import vader from './assets/vader.png'
 import cryptoClicker from './assets/cryptoclicker.png'
+import salescopy from './assets/salescopy.png'
+import musicplayer from './assets/musicplayer.png'
 
 export default [
     {
-        id: 2,
+        id: 6,
+        title: 'Music Player',
+        description: 'Musikspelare där du kan välja och lyssna på musik. Med ett användarnamn och lösenord går det också att ladda upp låtar till en databas som sedan visas i spellistan. Spellistan består av låtar som jag har varit med och skapat.',
+        img: musicplayer,
+        url: 'https://yellow-beach-02c09b903.5.azurestaticapps.net/',
+    
+        verktyg: [],
+        order: 0,
+        github: 'https://github.com/Louinielsendev/music-player'
+        
+    },
+    {
+        id: 5,
+        title: 'Salescopy.se',
+        description: 'Skapa Effektiva Annonser Snabbt och Enkelt med Salescopy.se Få dina annonser att sticka ut från mängden med vår avancerade plattform för att skapa annonstexter! Applikationen använder api från OpenAI som med hjälp av en produktbeskrivning generarar en passande annonstext för produkten.',
+        img: salescopy,
+        url: 'https://salescopy.vercel.app/',
+    
+        verktyg: [],
+        order: 1,
+        github: 'https://github.com/Louinielsendev/salescopy'
+        
+    },
+    {
+        id: 4,
         title: 'Crypto Clicker (PWA)',
         description: 'Spelet är enkelt där användaren kan trycka på ett virtuellt mynt för att generera fler mynt. Målet är att samla så många mynt som möjligt för att köpa olika skins och uppgraderingar som kan hjälpa användaren att generera fler mynt snabbare. Spelet har ett crypto-tema, så mynten som genereras och används för att köpa uppgraderingar representeras av digitala valutor som Bitcoin eller Ethereum. För Att spara användaren används localstorage och phpmyadmin, där ett id för varje webbläsare sparas i databasen.',
         img: cryptoClicker,
         url: 'https://melab.lnu.se/~ll223vk/cryptoClicker/',
     
-        verktyg: ['HTML', 'CSS', 'JavaScript', 'PHP', 'phpmyadmin'],
+        verktyg: [],
         order: 0,
         github: 'https://github.com/Louinielsendev/cryptoClicker'
         
@@ -23,7 +49,7 @@ export default [
         img: vader,
         url: 'https://glittery-sherbet-6df2ab.netlify.app/',
     
-        verktyg: ['HTML', 'CSS', 'JavaScript'],
+        verktyg: [],
         order: 1,
         github: 'https://github.com/Louinielsendev/v-der/'
         
@@ -34,7 +60,7 @@ export default [
         description: 'Catch Filip är ett roligt och snabbt spel där spelaren tar på sig rollen som en huvudjägare och målet är att fånga så många av Filips huvud som möjligt. Huvudena faller i olika hastigheter, vilket gör att spelaren måste vara snabb och uppmärksam för att fånga dem alla. Spelet blir mer utmanande ju längre spelaren spelar, ju längre spelet går, ju snabbare skapas nya huvden som spelaren måste fånga. (Använd A och D för att styra korgen)',
         img: catchFilip,
         url: 'https://catchfilip.netlify.app/',
-        verktyg: ['HTML', 'CSS', 'JavaScript'],
+        verktyg: [],
         order: 0,
         github: 'https://github.com/Louinielsendev/Catch-filip/'
     },
@@ -46,7 +72,7 @@ export default [
         img: blog,
         url: 'https://melab.lnu.se/~ll223vk/Blogg/index.php',
 
-        verktyg: ['HTML', 'CSS', 'JavaScript', 'PHP', ],
+        verktyg: [],
         order: 1,
         github: 'https://github.com/Louinielsendev/Blogg/',
         
